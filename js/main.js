6ed3e256aef80fbf01a5c06b5db58552e021a27c
Mario Gonzalez <mariocego99@gmail.com>
2022-06-22 18:51:33 -0300

    Entregable 2

--- js/main.js
+++ js/main.js
@@ -1,36 +1,44 @@
-let option = prompt("Eliga una opcion: \n A. Crear tabla de multiplicacion \n B. Repetir letra/frase").toUpperCase();
+let option = prompt("Eliga una opcion: \n A. Aplicar impuesto al producto \n B. Aplicar descuento al producto").toUpperCase();
 
 while(option != "ESC"){
     switch(option){
         case "A":
-            let dato = parseInt(prompt("Ingrese numero que desea para formar la tabla"));
-            
-                for(let i = 1 ; i <= 10 ; i++){
-                    let aux = dato + " X " + i + " = " + dato*i ;
-                    console.log(aux);
-                    mostrarEnPantalla(aux);
-
-                }
-                option = "ESC";
+            let datoA = parseInt(prompt("Ingrese valor del producto para aplicar el impuesto"));
+            aplicarImpuesto(datoA);
+            option = "ESC";
             break;
         case "B":
-                let word = prompt("Ingrese palabra a repetir");
-                let rep = parseInt(prompt("Ingrese cantidad de repeticiones"));
-                let aux = 0;
-                while(aux < rep){
-                    console.log(word);
-                    aux++;
-                    mostrarEnPantalla(word);
-                    
-                }
-                option = "ESC";
+            let datoB = parseInt(prompt("Ingrese valor del producto para aplicar el descuento"));
+            aplicarDescuento(datoB);
+            option = "ESC";
             break;
     }
 }
 
-function mostrarEnPantalla(contenido){
-    let p = document.createElement("p");
-    let container = document.getElementById("container");
-    p.innerHTML = contenido ;
-    container.appendChild(p);
+// Funcion para aplicar los impuesto a los productos
+function aplicarImpuesto(producto){
+    if(producto <= 100){
+        producto = producto + (producto * 0.1);
+        alert("El producto más impuesto del 10% es: " + producto);
+    }else if(producto <=1000){
+        producto = producto + (producto * 0.05);
+        alert("El producto más impuesto del 5% es: " + producto);
+    }else{
+        producto = producto + (producto * 0.03);
+        alert("El producto más impuesto del 3% es: " + producto);
+    }
+}
+
+// Funcion para aplicar el descuento a los productos
+function aplicarDescuento(producto){
+    if(producto <= 500){
+        producto = producto - (producto* 0.1);
+        alert("El producto con 10% de decuento queda en: " + producto);
+    }else if(producto <= 1500){
+        producto = producto - (producto* 0.15);
+        alert("El producto con 15% de decuento queda en: " + producto);
+    }else{
+        producto = producto - (producto* 0.2);
+        alert("El producto con 20% de decuento queda en: " + producto);
+    }
 }

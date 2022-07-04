c2230648e4b9d3110ef7f0927e84047f7c7e1043
Mario Gonzalez <mariocego99@gmail.com>
2022-07-04 11:53:13 -0300

    Primera entega

--- js/main.js
+++ js/main.js
@@ -1,55 +1,53 @@
 class Sistema{
     constructor(){
         this.listaEstudiantes = [];
+        this.estudiantesAprobados = [];
+        this.estudiantesReprobados = [];
     }
     agregarEstudiante(estudiante){
-        this.listaEstudiantes.push(estudiante)
+        this.listaEstudiantes.push(estudiante);
+        this.estudiantesReprobados.push(estudiante);
     }
     mostrarEstudiantes(){
         console.log("--- LISTA DE ESTUDIANTES ---");
         let cont = 1;
         for(let elem of this.listaEstudiantes){
-            console.log(cont + ") " + elem.nombre + " " + elem.apellido);
+            console.log(cont + ") " + "\n Nombre: " + elem.nombre + "\n Apellido: " + elem.apellido + "\n Cedula: " + elem.cedula);
             cont ++;
         }
     }
     mostrarEstudiantesAprobados(){
         let cont = 1;
         console.log("--- ESTUDIANTES APROBADOS ---");
-        let aux = true;
-        for(let elem of this.listaEstudiantes){
-            if(elem.aprobado){
-                console.log(cont + ") " + elem.nombre + " " + elem.apellido);
+        if(this.estudiantesAprobados != 0){
+            for(let elem of this.estudiantesAprobados){
+                console.log(cont + ") " + "\n Nombre: " + elem.nombre + "\n Apellido: " + elem.apellido);
                 cont ++;
-                aux = false;
             }
-        }
-        if(aux){
+        }else{
             console.log("NINGUN ESTUDIANTE APROBO");
         }
     }
     mostrarEstudiantesReprobados(){
         let cont = 1;
-        let aux = true;
         console.log("--- ESTUDIANTES REPROBADOS ---");
-        for(let elem of this.listaEstudiantes){
-            if(!elem.aprobado){
-                console.log(cont + ") " + elem.nombre + " " + elem.apellido);
+        if(this.estudiantesReprobados != 0){
+            for(let elem of this.estudiantesReprobados){
+                console.log(cont + ") " + "\n Nombre: " + elem.nombre + "\n Apellido: " + elem.apellido);
                 cont ++;
-                aux = false;
             }
-        }
-        if(aux){
+        }else{
             console.log("NINGUN ESTUDIANTE REPROBO");
         }
     }
     aprobarEstudiante(nombre, apellido){
         let aux = true;
-        for(let elem of this.listaEstudiantes){
+        for(let elem of this.estudiantesReprobados){
             if(elem.nombre.toUpperCase() == nombre && elem.apellido.toUpperCase() == apellido){
-                elem.aprobar();
+                this.estudiantesAprobados.push(elem);
+                this.estudiantesReprobados.splice(this.estudiantesReprobados.indexOf(elem), 1);
                 aux = false;
-                alert("El estudiante: " + elem.nombre + " " + elem.apellido + " Aprobo el curso");
+                alert("El estudiante: " + elem.nombre + " " + elem.apellido + " aprobo el curso");
             }
         }
         if(aux){
@@ -58,41 +56,47 @@ class Sistema{
     }
     reprobarEstudiante(nombre, apellido){
         let aux = true;
-        for(let elem of this.listaEstudiantes){
+        for(let elem of this.estudiantesAprobados){
             if(elem.nombre.toUpperCase() == nombre && elem.apellido.toUpperCase() == apellido){
-                elem.reprobar();
+                this.estudiantesReprobados.push(elem);
+                this.estudiantesAprobados.splice(this.estudiantesAprobados.indexOf(elem), 1);
                 aux = false;
-                alert("El estudiante: " + elem.nombre + " " + elem.apellido + " Reprobo el curso");
+                alert("El estudiante: " + elem.nombre + " " + elem.apellido + " reprobo el curso");
             }
         }
         if(aux){
             console.log("El estudiante no esta registrado, no se puede reprobar");
         }
     }
+    buscarEstudianteXApellido(apellido){
+        return this.listaEstudiantes.filter((estudiante) => estudiante.apellido.toUpperCase().includes(apellido.toUpperCase()));
+    }
+    buscarEstudianteXCedula(cedula){
+        return this.listaEstudiantes.find((estudiante) => estudiante.cedula === cedula);
+    }
+    porcentajeEstudiantesAprobados(){
+        return Math.round(((this.estudiantesAprobados.length * 100) / this.listaEstudiantes.length));
+    }
+    porcentajeEstudiantesReprobados(){
+        return Math.round(((this.estudiantesReprobados.length * 100) / this.listaEstudiantes.length));
+    }
 }
 class Estudiante{
-    constructor(nombre, apellido){
+    constructor(nombre, apellido, cedula){
         this.nombre = nombre;
         this.apellido = apellido;
-        this.aprobado = false;
-    }
-    aprobar(){
-        this.aprobado = true;
-    }
-    reprobar(){
-        this.aprobado = false;
+        this.cedula = cedula;
+        this.id = sistema.listaEstudiantes.length; 
     }
 }
 
-// Inicio el sistema 
 const sistema = new Sistema();
-
-// Funciones de crear, aprobar y reprobar estudiante
 
 function crearEstudiante(){
     let nombre = prompt("Ingrese nombre del estudiante");
     let apellido = prompt("Ingrese apellido del estudiante");
-    sistema.agregarEstudiante(new Estudiante(nombre, apellido));
+    let cedula = parseInt(prompt("Ingrese cedula del estudiante, sin puntos ni guion"));
+    sistema.agregarEstudiante(new Estudiante(nombre, apellido, cedula));
 }
 function aprobarEstudiante(){
     // EA = Estudiante Aprobado
@@ -107,9 +111,7 @@ function reprobarEstudiante(){
     sistema.reprobarEstudiante(nombreER, apellidoER);
 }
 
-// Comienzo del simulador
-
-console.log("INICIO \n A. Inscribir estudiante \n B. Aprobar estudiante \n C. Reprobar estudiante \n D. Mostrar todos los estudiantes inscripto \n E. Mostrar estudiante aprobados \n F. Mostrar estudiantes reprobado \n G. Limpiar consola \n ESC. Salir");
+console.log("INICIO \n A. Inscribir estudiante \n B. Aprobar estudiante \n C. Reprobar estudiante \n D. Mostrar todos los estudiantes inscripto \n E. Mostrar estudiante aprobados \n F. Mostrar estudiantes reprobado \n G. Buscar estudiantes por apellidos \n H. Comprobar que el estudiante este inscripto/a \n I. Porcentaje de estudiantes aprobados \n J. Porcentaje de estudiantes reprobados \n K. Limpiar consola \n ESC. Salir");
 let option = prompt("Eliga una opcion").toUpperCase();
 
 while(option != "ESC"){
@@ -139,8 +141,46 @@ while(option != "ESC"){
             option = "HOME";
             break;
         case "G":
+            let apellido = prompt("Ingrese el apellido del estudiante a buscar");
+            if(sistema.buscarEstudianteXApellido(apellido)[0] === undefined){
+                console.log("No se encontraron estudiantes con ese apellido");
+            }else{
+                console.log("--- ESTUDIANTES ENCONTRADOS ---");
+                for(let elem of sistema.buscarEstudianteXApellido(apellido)){
+                    console.log(elem.nombre + " " + elem.apellido );
+                }
+            }
+            option = "HOME";
+            break;
+        case "H":
+            let cedula = parseInt(prompt("Ingrese cedula del estudiante a buscar sin punto ni guion"));
+            if(sistema.buscarEstudianteXCedula(cedula) === undefined){
+                console.log("Estudiante no esa inscripto/a");
+            }else{
+                console.log("Estudiante encontrado/a")
+                console.log(sistema.buscarEstudianteXCedula(cedula).nombre + " " + sistema.buscarEstudianteXCedula(cedula).apellido );
+            }
+            option = "HOME";   
+            break;
+        case "I":
+            if(sistema.porcentajeEstudiantesAprobados() == 0){
+                console.log("Ningun estudiante aprobo");
+            }else{
+                console.log("El " + sistema.porcentajeEstudiantesAprobados() + "% de los estudiante aprobo");
+            }
+            option = "HOME";
+            break;
+        case "J":
+            if(sistema.porcentajeEstudiantesReprobados() == 0){
+                console.log("Ningun estudiante reprobo");
+            }else{
+                console.log("El " + sistema.porcentajeEstudiantesReprobados() + "% de los estudiante reprobo");
+            }
+            option = "HOME";
+            break;
+        case "K":
             console.clear();
-            console.log("INICIO \n A. Inscribir estudiante \n B. Aprobar estudiante \n C. Reprobar estudiante \n D. Mostrar todos los estudiantes inscripto \n E. Mostrar estudiante aprobados \n F. Mostrar estudiantes reprobado \n G. Limpiar consola \n ESC. Salir")
+            console.log("INICIO \n A. Inscribir estudiante \n B. Aprobar estudiante \n C. Reprobar estudiante \n D. Mostrar todos los estudiantes inscripto \n E. Mostrar estudiante aprobados \n F. Mostrar estudiantes reprobado \n G. Buscar estudiantes por apellidos \n H. Comprobar que el estudiante este inscripto/a \n I. Porcentaje de estudiantes aprobados \n J. Porcentaje de estudiantes reprobados \n K. Limpiar consola \n ESC. Salir");
             option = "HOME";
             break;
         case "HOME":
